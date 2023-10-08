ec83bf9341702b6abe5ee692a3e29052f0c147aa
Carlos Lazaro <carlos.e.lazaro@hotmail.com>
2023-10-08 00:51:13 -0300

    Correcciones varias

--- camomila_web/settings.py
+++ camomila_web/settings.py
@@ -154,8 +154,8 @@ EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
-EMAIL_HOST_USER=""
-EMAIL_HOST_PASSWORD=""
+EMAIL_HOST_USER="c4rl0s.l4z4r0@gmail.com"
+EMAIL_HOST_PASSWORD="mzqo ggyh arql yedn"
 
 
 MESSAGE_TAGS={
